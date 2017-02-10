c036c4b180c476d31d1bb2064a6f643674f733d7
thomasonw <al_thomason@iname.com>
2017-02-10 15:17:57 -0800

    Added signal-k support files
    Files which help in using the parser, as well as an example.

---
+++ signalk-support/README.md
@@ -0,0 +1,10 @@
+# Signal-k Support files
+
+
+This folder contains files often used with Signal-k.  
+Copy these files into your existing ```signalk-server-node``` folder.
+
+
+To run an example, use the following command from your signal-server-node directory:  ``  ./bin/osenergy-from-file``
+
+Then try opening a web browser and going to: ```http://localhost:3000/instrumentpanel/```
---
+++ signalk-support/signalk-server-node/providers/OSEnergy-signalk.js
@@ -0,0 +1,49 @@
+/*
+ * Origional work: Copyright 2015 Fabian Tollenaar <fabian@starting-point.nl>
+ * Modified work:  Copyright 2017 William (Al) Thomason <thomason.al@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var Transform = require('stream').Transform;
+
+function ToSignalK(options) {
+  Transform.call(this, {
+    objectMode: true
+  });
+
+  this.parser = new(require('OSEnergy-signalk').Parser)(options);
+
+  var that = this;
+  this.parser.on('OSEnergy', function(sentence) {
+    that.emit('OSEnergy', sentence)
+  });
+
+  this.parser.on('delta', function(delta) {
+    that.push(delta);
+  });
+}
+
+require('util').inherits(ToSignalK, Transform);
+
+ToSignalK.prototype._transform = function(chunk, encoding, done) {
+  try {
+    this.parser.write(chunk + '\n');
+  } catch (ex) {
+    console.error(ex);
+  }
+  done();
+}
+
+
+module.exports = ToSignalK;
